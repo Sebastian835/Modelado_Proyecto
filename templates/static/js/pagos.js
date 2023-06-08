f95fab724f8604f3fabb4bf2eae5a106703f0f0e
KevinStop <84851140+KevinStop@users.noreply.github.com>
2023-06-08 01:12:14 -0500

    sape

---
+++ templates/static/js/pagos.js
@@ -0,0 +1,17 @@
+function UpdatePago() {
+    var formulario = document.querySelector(".UpdatePago");
+    if (formulario.style.display === "none") {
+      formulario.style.display = "block";
+    } else {
+      formulario.style.display = "none";
+    }
+  }
+
+  function AgregarPago() {
+    var formulario = document.querySelector(".AgregarPago");
+    if (formulario.style.display === "none") {
+      formulario.style.display = "block";
+    } else {
+      formulario.style.display = "none";
+    }
+  }
